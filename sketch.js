79ba1a77c93e78a8e947e0fe8a0e321f4fe30542
Anisha Thiruselvam <thiruselvam.anisha@gmail.com>
2026-03-03 15:50:19 -0500

    fixed

--- sketch.js
+++ sketch.js
@@ -1,54 +1,28 @@
 let player;
-let worldImg;
+
+function preload() {
+  preloadWorld();
+}
 
 function setup() {
   createCanvas(800, 600);
-  textFont("Patrick Hand");
   player = new Player();
-}
-
-// Preload the world image before anything else
-function preload() {
-  worldImg = loadImage("assets/images/worldBackground.png");
+  textFont("Patrick Hand");
 }
 
 function draw() {
-  if (gameState === "start") {
-    drawStartScreen();
-  } else if (gameState === "characterSelect") {
-    drawCharacterSelect();
-  } else if (gameState === "world") {
-    drawWorldScreen();
+  if (gameState === "start") drawStartScreen();
+  else if (gameState === "characterSelect") drawCharacterSelect();
+  else if (gameState === "world") {
+    drawWorld();
+    player.move();
     player.display();
-    playerMovement();
-  }
+  } else if (gameState === "store") drawStore();
 }
 
-// ---------------- World Screen ----------------
-function drawWorldScreen() {
-  if (worldImg) {
-    image(worldImg, 0, 0, width, height);
-  } else {
-    background(200); // fallback if image not loaded
-  }
-}
-
-// ---------------- Player Movement ----------------
-function playerMovement() {
-  if (keyIsDown(LEFT_ARROW)) player.x -= player.speed;
-  if (keyIsDown(RIGHT_ARROW)) player.x += player.speed;
-  if (keyIsDown(UP_ARROW)) player.y -= player.speed;
-  if (keyIsDown(DOWN_ARROW)) player.y += player.speed;
-
-  player.x = constrain(player.x, 0, width - player.size);
-  player.y = constrain(player.y, 0, height - player.size);
-}
-
-// ---------------- Key Handling ----------------
 function keyPressed() {
-  if (gameState === "start" && key === " ") {
-    gameState = "characterSelect";
-  } else if (gameState === "characterSelect") {
+  if (gameState === "start" && key === " ") gameState = "characterSelect";
+  if (gameState === "characterSelect") {
     if (key === "1") {
       selectedCharacter = "boy";
       player.setCharacter("boy");
